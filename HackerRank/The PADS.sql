225b48ca254a25c6bfb4b7aa49eda40d5ed59b7b
김태현 <xogusrla7@gmail.com>
2025-05-12 17:25:17 +0900

    !

---
+++ HackerRank/The PADS.sql
@@ -0,0 +1,8 @@
+SELECT Name || '(' || substr(Occupation, 1, 1) || ')'
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT 'There are a total of ' || COUNT(*) || ' ' || LOWER(Occupation) || 's.'
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*), Occupation;
